e5b5346428d382c8bf4b96f7dae3335cf575d4df
koerino <koerino@gmail.com>
2016-03-08 10:32:44 -0500

    modify folder structure to use direct CRUD operations, add views

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,4 @@
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app=Flask(__name__)
 app.config.from_object('config')
-
-db = SQLAlchemy(app)
-
- @app.errorhandler(404)
-    def not_found(error):
-        return render_template('404.html'), 404
--- application/views.py
+++ application/views.py
@@ -1,1 +1,5 @@
 #Routing and render templates
+
+ #@app.errorhandler(404)
+    #def not_found(error):
+        #return render_template('404.html'), 404
