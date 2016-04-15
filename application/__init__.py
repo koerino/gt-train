7519aa80923bcadf836c6799170e9f43663a337b
koerino <koerino@gmail.com>
2016-04-15 13:27:51 -0400

    update configs and python main module

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
+import pymysql
 
 app=Flask(__name__)
+app.config.from_pyfile('config.cfg')
 
-#app.config.from_object('config')
+db = pymysql.connect(host=app.config['HOST'], user=app.config['USER'], passwd=app.config['PWD'], db=app.config['DB'])
+cursor = db.cursor()
+
+sql = """ CREATE TABLE people
+               (id INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+                name TEXT NOT NULL,
+                email TEXT,
+                age INTEGER)"""
+
+cursor.execute(sql)
+
+#sql = """INSERT INTO Manager (Username) VALUES (%s)"""
+#cursor.execute(sql, ("manger"))
+#cursor.close()
 
 @app.route('/')
 @app.route('/<path:path>')
